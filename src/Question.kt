676f7dad7b40a3e40649d719aa2ea7503703c20f
Feaxsail <shher.dan.99@mail.ru>
2026-01-28 15:05:20 +0300

    Use a singleton object

--- src/Question.kt
+++ src/Question.kt
@@ -1,3 +1,4 @@
+
 data class Question<T>(
     val questionText: String,
     val answer: T,
@@ -8,20 +9,66 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-fun main() {
+object StudentProgress {
+    var answered: Int = 0
+    var total: Int = 10
+}
+
+class Quiz1 {
     val question1 = Question<String>(
         "Речка спятила с ума – По домам пошла сама. ___",
         "водопровод",
         Difficulty.MEDIUM
     )
 
-    println(question1.toString())
+    val question2 = Question<Boolean>(
+        "Солнце встаёт на востоке.",
+        true,
+        Difficulty.EASY
+    )
 
-    val question1Copy = Question<String>(
+    val question3 = Question<Int>(
+        "Сколько будет 2 + 2?",
+        4,
+        Difficulty.EASY
+    )
+}
+
+
+class Quiz {
+    companion object StudentProgress {
+        var answered: Int = 3
+        var total: Int = 10
+    }
+
+    val question1 = Question<String>(
         "Речка спятила с ума – По домам пошла сама. ___",
         "водопровод",
         Difficulty.MEDIUM
     )
 
-    println("Равны ли объекты? ${question1 == question1Copy}")
+    val question2 = Question<Boolean>(
+        "Сколько будет 2 + 2?",
+        true,
+        Difficulty.EASY
+    )
+
+    val question3 = Question<Int>(
+        "Солнце встаёт на востоке.",
+        4,
+        Difficulty.EASY
+    )
+}
+
+fun main() {
+    println("=== ШАГ 10: Singleton и companion object ===")
+
+    println("До переноса: ${StudentProgress.answered} of ${StudentProgress.total} answered.")
+
+    println("После переноса: ${Quiz.answered} of ${Quiz.total} answered.")
+
+    val quiz = Quiz()
+    println("Первый вопрос: ${quiz.question1.questionText}")
+
+
 }
