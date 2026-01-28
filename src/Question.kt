1964c8700fc88c2d06cb94e0896cc4e2761b8a2d
Feaxsail <shher.dan.99@mail.ru>
2026-01-28 14:43:27 +0300

    Initial classes with code duplication

---
+++ src/Question.kt
@@ -0,0 +1,17 @@
+class StringQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+
+class BooleanQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+
+class IntQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
